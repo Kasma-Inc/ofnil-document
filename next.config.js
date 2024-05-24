0eab5aecc6b843241fe12f047ad56e7204ce7a80
zuiidea <zuiiidea@gmail.com>
2024-05-24 11:46:14 +0800

    ⚡️  improve deploy

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withNextra = require('nextra')({
 });
 
 module.exports = withNextra({
+  basePath: process.env.BASE_PATH,
   i18n: {
     locales: ['en-US', 'zh-CN'],
     defaultLocale: 'zh-CN',
